7deb8f8d766dc7826b81e321e5dc6cf8633aa39d
Fran <fran.ap89@gmail.com>
2016-07-22 19:39:08 +0200

    Vistas universidades y posts realizads con las cards

--- js/app.js
+++ js/app.js
@@ -1,28 +1,59 @@
-var app = angular.module('Eras+App', ['ngRoute']);
+var app = angular.module('Eras+App', ['ngRoute','LocalStorageModule']);
 
-app.config(['$routeProvider',function($routeProvider){
-	$routeProvider
-		.when('/', {
-			templateUrl: 'views/portada.html',
-			controller: 'FirstController'
-		})
-		.when('/inicio', {
-			templateUrl: 'views/inicio.html',
-			controller: 'FirstController'
-		})
-		.when('/estudiar', {
-			templateUrl: 'views/estudiar.html',
-			controller: 'studiesController'
-		})
-		.when('/posts', {
-			templateUrl: 'views/posts.html',
-			controller: 'PostController'
-		})
-		.otherwise({
-			redirectTo: '/'
-		})
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/portada.html',
+            controller: 'Universities'
+        })
+        .when('/inicio', {
+            templateUrl: 'views/inicio.html',
+            controller: 'Universities'
+        })
+        .when('/estudiar', {
+            templateUrl: 'views/estudiar.html',
+            controller: 'Universities'
+        })
+        .when('/posts', {
+            templateUrl: 'views/posts.html',
+            controller: 'Posts'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}]);	
+
+app.controller('Universities', ['$scope', '$http', function($scope, $http) {
+    $http.get("json/universidades.json").success(function(data) {
+        $scope.universidades = data;
+
+      
+    });
+
 }]);
 
-app.controller('FirstController', ['$scope', function($scope){
-	$scope.text = 'Merengue Merengueeee!!';
+app.controller('Posts', ['$scope', '$http', function($scope, $http) {
+    $http.get("json/posts.json").success(function(data) {
+        $scope.posts = data;
+
+      });
+
 }]);
+
+
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+});
+
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+
+/*  $scope.facultades = function () {
+        	for (i = 0; i < universidades.length; i++) {
+        		var facultades = 
+        	}
+        }
+*/
